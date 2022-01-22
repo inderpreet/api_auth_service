bfaa7128b15c1e705703e96e5d22e64dc6fd68e2
Inderpreet Singh <inderpreet.v1@gmail.com>
2022-01-22 11:51:02 -0500

    add mongoose connectivity

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 // import routes
 const authRoute = require("./routes/auth");
 
+dotenv.config()
 
-// route middleware
+// connect to db
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, ()=>{
+    console.log("Connected to DB");
+});
+
+
+// Other middleware
+app.use(express.json());
+
+// Routes middleware
 app.use("/api/user", authRoute);
-
 
 app.listen(3000, ()=> console.log("Server running at 3000"));
---
+++ model/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 25,
+        min: 6
+    },
+    password:{
+        type: String,
+        required: true,
+        max: 1024,
+        mix: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+},
+{
+    collection:"users"
+});
+
+module.exports = mongoose.model("User", userSchema);
